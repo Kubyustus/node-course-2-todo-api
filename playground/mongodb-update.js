8ff71f5069a13fc8418486a5c038f86ef2b71e38
Neriad Hakak <neriad@gmail.com>
2017-03-08 16:48:56 +0200

    add post /todos route and refactor mongoose

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,34 +1,38 @@
-//const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-        return console.log('Unable to connect to MongoDB server');
-    }    
-    console.log('Connected to MongoDB server');
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    //db.collection('Todos').findOneAndUpdate({
-    //    _id: new ObjectID('589b03c1efdcce8b89071fce')
-    //}, {
-    //    $set: {
-    //        completed: true
-    //    }
-    //}, {
-    //    returnOriginal: false
-    //}).then((result) => {
-    //    console.log(result);
-    //});
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
-    db.collection('Users').findOneAndUpdate({
-        _id: new ObjectID('58973af48a309222b416ad14')
-    }, {
-        $set: { name: 'Neriad' },  
-        $inc: { age: 1 }        
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(result);
-    });  
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
 
-    //db.close();
+  // db.close();
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -11,47 +10,45 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
+    //console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-// GET /todos/123434
-app.get('/todos/:id', (req, res) => {
-  //res.send(req.params);
-  var id = req.params.id;
-
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-  
-  Todo.findById(id).then((todo) => {
-    if(!todo) {
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
     });
 });
 
+
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+    console.log('Started on port 3000');
 });
 
-module.exports = {app};
+/*
+var newTodo2 = new Todo({
+    text: 'Feed The cat'
+});
+
+newTodo2.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save todo', e);
+});
+*/
+
+
+
+/*
+var newUser1 = new User({
+    email: 'test1@test.com'
+});
+
+newUser1.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save todo', e);
+});
+*/
